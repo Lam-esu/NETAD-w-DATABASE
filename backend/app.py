dd19d9457bfccd8201241c6f65c7b79d84ab6c67
Lam-esu <lamuelalojado238@gmail.com>
2026-05-26 19:21:47 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ from security import (
 from camera import CameraService
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "frontend"))
+FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "frontend"))
 
 # =====================================================
 # LOAD ENV VARIABLES
